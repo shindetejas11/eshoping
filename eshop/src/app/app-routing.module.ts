529b09c6e2d703eff4d86cdcd800ad15c72b3caa
shindetejas11 <110617094+shindetejas11@users.noreply.github.com>
2022-10-22 23:28:00 +0530

    Component added

--- eshop/src/app/app-routing.module.ts
+++ eshop/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/Home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'Home',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
